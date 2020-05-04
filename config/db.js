0cf27e2e1a513aa7bcef80c6b3a3484bfe490529
Jake Kravas <jakekravas7@gmail.com>
2020-05-03 23:05:22 -0700

    added update and delete routes for contacts

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get("mongoURI");
 // Same thing as below, just looks cleaner
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || db, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGODB_URI || db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
     console.log("MongoDB Connected...");
   } catch (err ) {
     console.error(err.message);
