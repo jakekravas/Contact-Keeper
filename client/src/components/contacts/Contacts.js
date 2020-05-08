9272d5b7b4576b01318638cba892ae261e692fb8
Jake Kravas <jakekravas7@gmail.com>
2020-05-08 13:51:48 -0700

    can now get and clear contacts with back-end implementation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,34 +1,40 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactItem from "./ContactItem";
 import ContactContext from "../../context/contact/contactContext";
+import Spinner from "../../components/layout/Spinner"
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-  if (contacts.length === 0){
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  if (contacts === null){
+    return <Spinner/>
+  } else if (contacts.length === 0){
     return <h4>Please add a contact</h4>
   }
 
   return (
     <Fragment>
-      <TransitionGroup>
+      {contacts !== null && !loading ? (<TransitionGroup>
         {/* If filtered isn't empty, map through filtered, else map through contacts */}
         {filtered !== null ? filtered.map(contact =>(
-          <CSSTransition key={contact.id} timeout={500} classNames="item">
-            <ContactItem key={contact.id} contact={contact}/>
+          <CSSTransition key={contact._id} timeout={500} classNames="item">
+            <ContactItem contact={contact}/>
           </CSSTransition>
           )
         ) : contacts.map(contact =>(
-          <CSSTransition key={contact.id} timeout={500} classNames="item">
+          <CSSTransition key={contact._id} timeout={500} classNames="item">
             <ContactItem contact={contact}/>
           </CSSTransition>
         )
         )}
-      </TransitionGroup>
-      
+      </TransitionGroup>) : <Spinner/>}
     </Fragment>
   )
 }
--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,19 +1,29 @@
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
+  CONTACT_ERROR,
+  CLEAR_CONTACTS
 } from "../types";
 
 export default (state, action) => {
   switch(action.type){
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false
+      }
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
+        contacts: [...state.contacts, action.payload],
+        loading: false
       }
     case UPDATE_CONTACT:
       return {
@@ -21,12 +31,22 @@ export default (state, action) => {
         contacts: state.contacts.map(
         // if the contact's id that we're iterating thru is equal to the one in the payload, set that element of the array to the contact object in the payload, otherwise set the element to the contact as is
         contact => contact.id === action.payload.id ? action.payload : contact
-        )
+        ),
+        loading: false
       }
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(contact => contact.id !== action.payload),
+        loading: false
+      }
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        error: null,
+        current: null
       }
     case SET_CURRENT:
       return {
@@ -53,6 +73,11 @@ export default (state, action) => {
     return {
       ...state,
       filtered: null
+    }
+    case CONTACT_ERROR:
+    return {
+      ...state,
+      error: action.payload
     }
 
     default: return state;
