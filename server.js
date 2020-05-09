c586e458caa002855eef1fe164b716f11a868f61
Jake Kravas <jakekravas7@gmail.com>
2020-05-08 22:16:15 -0700

    fixed typo in port variable assignment

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production"){
   );
 }
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server up at http://localhost:${PORT}`);
