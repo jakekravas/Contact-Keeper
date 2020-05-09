9961bba8e77d5969c80943dd3dc893545682522c
Jake Kravas <jakekravas7@gmail.com>
2020-05-08 22:06:17 -0700

    attempt to fix deployment issue

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 connectDB();
 
 // Init Middleware
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.json({ extended: false }));
 
 // Define routes
 app.use("/api/users", require("./routes/users"));
@@ -22,7 +22,9 @@ if (process.env.NODE_ENV === "production"){
   // Set static folder
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 const PORT = process.env.port || 5000;
